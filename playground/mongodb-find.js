94e322785f32a99e6effc37b3b909a0c33e18b34
timtnguyen <nguyen710150@yahoo.com>
2018-06-22 22:15:46 -0700

    Another update

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -30,7 +30,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
         console.log('Users'); 
         console.log(JSON.stringify(docs, undefined, 2)); 
     }, (err) => {
-        console.log('Unable to fetch users', err); 
+        console.log(`Unable to fetch ${err}`); 
     }); 
 
     //client.close(); // connect to mongodb 
