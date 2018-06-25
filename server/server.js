5ff38a3afde57701ee149f0ffce54cb816c3788c
timtnguyen <nguyen710150@yahoo.com>
2018-06-24 23:33:11 -0700

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const {ObjectID} = require('mongodb'); 
+
 let express = require('express'); 
 let bodyParser = require('body-parser'); 
 
@@ -27,6 +29,22 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e); 
     });
+});
+
+app.get('/todos/:id', (req, res) => {
+    let id = req.params.id; 
+
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send(); 
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            res.status(404).send(); 
+        } 
+
+        res.send({todo}); 
+    }).catch((e) => res.status(400).send());
 });
 
 
